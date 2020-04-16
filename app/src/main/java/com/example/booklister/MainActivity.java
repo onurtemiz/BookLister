b54157b81fb9e3dcb1a3659ecb7f935a13d6d4dd
Onur Temiz <onurmain@gmail.com>
2020-04-17 01:30:45 +0300

    Simple ArrayAdapter Implementation

--- app/src/main/java/com/example/booklister/MainActivity.java
+++ app/src/main/java/com/example/booklister/MainActivity.java
@@ -18,7 +18,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.book_list);
-        books.add(new Book("The Name of the Wind","Fantasy","800",0));
+        books.add(new Book("The Name Of The Wind","Fantasy","800",0));
+        books.add(new Book("A Dance With Dragon","Fantasy","757",0));
+        books.add(new Book("A Feast For Crows","Fantasy","538",0));
+        books.add(new Book("A Game Of Thrones","Fantasy","897",0));
+        books.add(new Book("A Storm Of Swords","Fantasy","678",0));
+        books.add(new Book("Harry Potter And The Goblet Fire","Fantasy","423",0));
+        books.add(new Book("Harry Potter And The Order Of The Phoenix","Fantasy","653",0));
+        books.add(new Book("Harry Potter And The Philosopher's Stone","Fantasy","231",0));
+        books.add(new Book("The World Of Ice And Fire","Fantasy","582",0));
         BookAdapter bookAdapter = new BookAdapter(this,books);
         ListView listView = (ListView) findViewById(R.id.bookListView);
         listView.setAdapter(bookAdapter);
